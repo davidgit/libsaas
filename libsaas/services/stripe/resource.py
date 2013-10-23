c12b9bbac038e3059439c03e364c86d45a1a3663
David Gay i Tello <david.gay.tello@gmail.com>
2013-10-23 15:00:06 +0200

    stripe: add count method collections
    So, we are avoiding paging.

--- libsaas/services/stripe/resource.py
+++ libsaas/services/stripe/resource.py
@@ -1,5 +1,15 @@
+import json
+
 from libsaas import http, parsers
 from libsaas.services import base
+
+
+def parse_count(body, code, headers):
+    if not 200 <= code < 300:
+        raise http.HTTPError(body, code, headers)
+
+    response = json.loads(body.decode('utf-8'))
+    return response['count']
 
 
 class StripeResource(base.RESTResource):
@@ -38,3 +48,20 @@ class ListResourceMixin(object):
         request = http.Request('GET', self.get_url(), params)
 
         return request, parsers.parse_json
+
+    @base.apimethod
+    def count(self, *args, **kwargs):
+        """
+        Fetch an integer count of the number of objects of a collection. This
+        is an absolute number, regardless of paging limits, so use this if you
+        want to tally up a collection instead of iterating through all of its
+        objects.
+
+        Accepts the same arguments as `get`.
+        """
+        with base.extract_request():
+            kwargs['count'] = 1
+            request = self.get(*args, **kwargs)
+
+        return request, parse_count
+
